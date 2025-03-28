142ef8348223ac359b91ef42477ef8abf60ab180
qkrwnstn <aaaji0910@gmail.com>
2025-03-28 16:16:22 +0900

    ㅓㅠㅗㅑㅠㅑㅕ

--- src/test/GitFirst.java
+++ src/test/GitFirst.java
@@ -3,9 +3,12 @@ package test;
 public class GitFirst {
 
 	public static void main(String[] args) {
+		int adf =121;
 		System.out.println("1.깃허브연동");
 		System.out.println("두번째실험");
 		System.out.println("3번째실험");
+		System.out.println(adf);
+		System.out.println(adf);
 	}
 
 }
