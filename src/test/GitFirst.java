ef958e72cb5f6c21f9ba08fdc1663d53fad4affc
qkrwnstn <aaaji0910@gmail.com>
2025-03-28 16:19:22 +0900

    집에서 작업

--- src/test/GitFirst.java
+++ src/test/GitFirst.java
@@ -6,9 +6,11 @@ public class GitFirst {
 		int adf =121;
 		System.out.println("1.깃허브연동");
 		System.out.println("두번째실험");
-		System.out.println("3번째실험");
+		System.out.println("3번째실험412412");
+		System.out.println("집에서 작업");
 		System.out.println(adf);
 		System.out.println(adf);
+		
 	}
 
 }
